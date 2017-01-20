1af7dd0fead2d3f80b977313f91dda851a9e16df
kalle (jag) <kalle.jillheden@me.com>
2017-01-20 20:29:32 +0100

    Some utility
    + Added utility scripts. For example extension methods and sinus rotator
    (if we eeeeeeeever need it ¯\_(ツ)_/¯).
    + Changed assets serialization to "force text"
    + Changed the .gitignore to ignore visual studio generated files.

---
+++ GGJ17/Assets/Scripts/Utility/Attributes/StringOptions.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+[AttributeUsage(AttributeTargets.Field)]
+public class StringOptionsAttribute : PropertyAttribute {
+
+	public string[] options;
+
+	public StringOptionsAttribute() {
+		this.options = new string[] { "-" };
+	}
+
+	public StringOptionsAttribute(string[] options) {
+		this.options = options;
+	}
+
+}
