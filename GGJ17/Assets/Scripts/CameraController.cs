afe0e73149448abd026a8665e40d6a852fa9ea6b
kalle (jag) <kalle.jillheden@me.com>
2017-01-20 20:58:05 +0100

    :camera:
    + Gained information that you can use emojis in commit message :wolf:
    + Added camera script, and it's so smooooooooooth.

---
+++ GGJ17/Assets/Scripts/CameraController.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+	
+	/*
+		A programmer once stated that when it comes to comments in code,
+		your variable and function names should be enough.
+		With the exception for complex forumlas..
+		Not everyone knows pythagoras theorym.
+
+		Well, everyone does, but you get the point.
+
+		My descriptive names are at least better than foo bar x)
+	*/
+
+	public Transform player;
+	[Range(0,1)]
+	public float scaleForCameraFollowingMultiplierIfSetToOneItFollowsDirectlyIfSetToAHalfItOnlyFollowsHalfWayItIsSimple = 1;
+	public float speedInWichTheCameraFollowsThePlayerSoThatItInterpolatesInANiceAndSmoothFashion = 5;
+
+	private Vector3 offsetFromPlayer;
+	private Vector3 centerPosition;
+
+	private void Start() {
+		offsetFromPlayer = transform.position - player.position;
+		centerPosition = transform.position;
+	}
+
+	private void LateUpdate() {
+		var ultimateTarget = player.position + offsetFromPlayer;
+		var scaledTarget = Vector3.Lerp(centerPosition, ultimateTarget, scaleForCameraFollowingMultiplierIfSetToOneItFollowsDirectlyIfSetToAHalfItOnlyFollowsHalfWayItIsSimple);
+		var smoothedTarget = Vector3.Lerp(transform.position, scaledTarget, Time.deltaTime * speedInWichTheCameraFollowsThePlayerSoThatItInterpolatesInANiceAndSmoothFashion);
+
+		transform.position = smoothedTarget;
+	}
+
+}
