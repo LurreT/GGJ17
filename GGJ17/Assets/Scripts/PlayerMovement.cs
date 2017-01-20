fe1820c7057e605187dfaf3ba65732a7581ef811
LAPTOP-EL5JQDRC\Lokas <lukas.tornqvist95@gmail.com>
2017-01-20 20:32:12 +0100

    started on movement

---
+++ GGJ17/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+
+	Rigidbody rb;
+	public float speed;
+	public float jumpForce;
+
+	void Start(){
+		rb = GetComponent<Rigidbody> ();
+
+	}
+
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		print (Input.GetAxis ("Horizontal"));
+
+		if (rb.velocity.z < speed) {
+			rb.velocity += new Vector3 (0, 0, Input.GetAxis ("Vertical")) * speed;
+		} else {
+			rb.velocity = new Vector3 (0, 0, Input.GetAxis ("Vertical")) * speed;
+		}
+		if (rb.velocity.x < speed) {
+			rb.velocity += new Vector3 (Input.GetAxis ("Horizontal"), 0,0) * speed;
+		}
+
+		print (Input.GetAxis ("Jump"));
+
+		if (Input.GetAxis ("Jump") > 0.5f) {
+			print ("yas");
+			rb.AddForce (Vector3.up * jumpForce);
+		}
+
+	}
+}
