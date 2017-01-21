1bc8728f40b2df0b1d838e85d5b92db831c8415d
LAPTOP-EL5JQDRC\Lokas <lukas.tornqvist95@gmail.com>
2017-01-21 12:27:55 +0100

    fetching changes in SelfDestuctUnit

--- GGJ17/Assets/Scripts/ShockWave.cs
+++ GGJ17/Assets/Scripts/ShockWave.cs
@@ -5,11 +5,21 @@ using UnityEngine;
 public class ShockWave : MonoBehaviour {
 
 	PlayerHealth ph;
+	public float selfDestructIn;
+
+	void Start(){
+		StartCoroutine (SelfDestruct ());
+	}
 
 	void OnTriggerEnter(Collider col){
 		ph = col.gameObject.GetComponent<PlayerHealth> ();
 		if (ph != null) {
 			ph.Damage ();
 		}
+	}
+
+	IEnumerator SelfDestruct(){
+		yield return new WaitForSeconds (selfDestructIn);
+		Destroy (gameObject);
 	}
 }
