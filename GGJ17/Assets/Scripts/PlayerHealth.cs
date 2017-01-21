98553d4fb0565ed5f13d65d2b67733e4fa49f264
LAPTOP-EL5JQDRC\Lokas <lukas.tornqvist95@gmail.com>
2017-01-21 11:18:57 +0100

    movement done

---
+++ GGJ17/Assets/Scripts/PlayerHealth.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerHealth : MonoBehaviour {
+
+	public int health;
+	public Image heart;
+
+
+	void Start(){
+		
+	}
+
+	public void Damage(){
+		health--;
+		if (health == 0) {
+			Death ();
+		}
+	}
+	void Death(){
+		GetComponent<PlayerMovement> ().enabled = false;
+		GetComponent<Rigidbody> ().constraints = RigidbodyConstraints.None;
+	}
+}
